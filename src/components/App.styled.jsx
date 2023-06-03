5d72fb7d6b20a9c625596233e2fc78c59dd3bd61
Inna-Melnyk <inamelnyk@ukr.net>
2023-06-03 13:32:23 +0200

    Update App.styled.jsx

--- src/components/App.styled.jsx
+++ src/components/App.styled.jsx
@@ -2,11 +2,5 @@ import styled from "@emotion/styled";
 
 export const Container = styled.div`
   min-width: 320px;
-   max-width: 1158px;
-   margin-left: auto;
-  margin-right: auto;
-  padding: 40px 16px;
-
-
   background-color: #e5ecf1;
 `;
